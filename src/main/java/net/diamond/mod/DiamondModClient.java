8c6a9217c9d8f404cf727905aa5f1d9f7376cd06
GoofyAhhProductionsOffical <shayaankamal09@gmail.com>
2024-06-08 05:15:12 -0500

    Initial commit

---
+++ src/main/java/net/diamond/mod/DiamondModClient.java
@@ -0,0 +1,10 @@
+package net.diamond.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DiamondModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
